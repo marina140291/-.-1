bfd3e877042a3d2797625fb2cb788ef907244115
marina140291 <marina140291@mail.ru>
2022-12-20 15:09:45 +0300

    Задача 2

---
+++ задача 2.cs
@@ -0,0 +1,17 @@
+﻿
+
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = a;
+int min = b;
+
+if (a > b) max = a; min = b;
+if (b > a) max = b; min = a;
+Console.Write("max= ");
+Console.WriteLine(max);
+Console.Write("min= ");
+
+Console.WriteLine(min);
