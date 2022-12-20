f747f3965948db1bb0289aebd62adbc37b7689fa
marina140291 <marina140291@mail.ru>
2022-12-20 15:25:29 +0300

    Задача 4

---
+++ задачи/задача 4/задача 4.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+
+
+
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+
+if (a > max) max = a; 
+if (b > max) max = b; 
+if (c > max) max = c;
+Console.Write("max= ");
+Console.WriteLine(max);
